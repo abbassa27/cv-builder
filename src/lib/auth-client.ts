11477f16c8d502d1b81c8c7d7a7210de52253e72
abbassa27 <abbassamalik@gmailcom>
2026-05-01 20:13:23 +0000

    update v2

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,4 +1,8 @@
 ﻿import { createAuthClient } from "better-auth/react"
+
 export const authClient = createAuthClient({
-  baseURL: process.env.NEXT_PUBLIC_APP_URL ?? "http://localhost:3000",
+  baseURL:
+    process.env.NODE_ENV === "production"
+      ? "https://cv-builder-one-flame.vercel.app"
+      : "http://localhost:3000",
 })
